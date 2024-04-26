358dc5f881057dc03380c204a56e98674b27f5ae
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2024-04-26 17:35:29 +0100

    Configure web service and endpoint

---
+++ src/main/java/com/example/SOAPCRUD/PersonEndpoint.java
@@ -0,0 +1,23 @@
+package com.example.SOAPCRUD;
+
+import io.spring.guides.gs_producing_web_service.GetPersonRequest;
+import io.spring.guides.gs_producing_web_service.GetPersonResponse;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class PersonEndpoint {
+    private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getPersonRequest")
+    @ResponsePayload
+    public GetPersonResponse getHello(@RequestPayload GetPersonRequest request) {
+        GetPersonResponse response = new GetPersonResponse();
+        response.setName(request.getName());
+        response.setAge(request.getAge());
+        response.setCity(request.getCity());
+        return response;
+    }
+}
